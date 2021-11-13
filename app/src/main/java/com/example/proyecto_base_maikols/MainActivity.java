5590146e71d054c24537bf4ad49a8208ad09c678
Maikol-s <rmaikol737@gmail.com>
2021-11-13 04:06:43 -0300

    Creación del Login y Menu principal Funcional. Creación del Insumos_Act con el Spinner Funcional pero falta colocar funcional el Boton.

--- app/src/main/java/com/example/proyecto_base_maikols/MainActivity.java
+++ app/src/main/java/com/example/proyecto_base_maikols/MainActivity.java
@@ -2,13 +2,83 @@ package com.example.proyecto_base_maikols;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import Objetos.Administrador;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText usuario,password;
+    private TextView mensaje;
+    private Administrador adm = new Administrador();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usuario = (EditText) findViewById(R.id.user);
+        password = (EditText) findViewById(R.id.pass);
+        mensaje = (TextView) findViewById(R.id.msj);
     }
+
+
+    public void IniciarSesion(View view){
+        String userObj = adm.getUser().toString().trim();
+        String passObj = adm.getPass().toString().trim();
+
+        String usu = usuario.getText().toString().trim();
+        String contraseña = password.getText().toString().trim();
+
+        switch (usu)
+        {
+            case "Maikol":
+                if (usu.equals(userObj) && contraseña.equals(passObj)){
+                    mensaje.setText("");
+                    Toast.makeText(this,"Sesion Iniciada", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(this, Home_Act.class);
+                    startActivity(i);
+                }
+                break;
+
+            case "":
+                if (usu.equals("") && contraseña.equals("")){
+                    mensaje.setText("Por Favor Rellene los Campos");
+                }
+                break;
+
+            default:
+                if (!usu.equals(userObj) && !contraseña.equals(passObj)){
+                    mensaje.setText("Algunos de los Campos son Incorrecto");
+                }
+                break;
+
+        }
+
+    }
+    public void CerrarApp(View view){
+        finish();
+    }
+    public void Facebook(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse("https://www.facebook.com/"));
+        startActivity(i);
+    }
+    public void Youtube(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse("https://www.youtube.com/"));
+        startActivity(i);
+    }
+    public void Twitter(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse("https://www.twitter.com/"));
+        startActivity(i);
+    }
+
+
 }
